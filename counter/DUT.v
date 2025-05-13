1421b33f82b1b0a3aa9db2f3354b932c3663867d
Sharanya1811 <85345072+Sharanya1811@users.noreply.github.com>
2025-05-13 20:26:29 +0530

    counter_dut.v

--- counter/DUT.v
+++ counter/DUT.v
@@ -1,1 +1,19 @@
+module counter4bit (
+  input  logic        clk,
+  input  logic        rst,
+  input  logic        enable,
+  input  logic        load,
+  input  logic [3:0]  load_val,
+  output logic [3:0]  count_val
+);
 
+  always @(posedge clk or posedge rst) begin
+    if (rst)
+      count_val <= 0;
+    else if (load)
+      count_val <= load_val;
+    else if (enable)
+      count_val <= count_val + 1;
+  end
+
+endmodule
